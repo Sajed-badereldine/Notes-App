1474c127f181640a4d4ff80c1fd4a95e0f0caef7
Sajed Badreddine <sajedbadereldine@gmail.com>
2026-05-18 13:40:26 +0300

    adding env file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'benzina.kereo.online';
+const API_BASE_URL = process.env.API_BASE_URL || 'benzina.kereo.online/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
