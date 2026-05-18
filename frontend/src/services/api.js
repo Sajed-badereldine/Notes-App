e0a68b6a03dd84df9502ec7cbd35ab1bcba3e896
Sajed Badreddine <sajedbadereldine@gmail.com>
2026-05-18 13:52:03 +0300

    editing url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'benzina.kereo.online/api';
+const API_BASE_URL = process.env.API_BASE_URL || '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
